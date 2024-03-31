385dc967c87d804f9d16663ab86df2c8ac373c89
Van Hoang TRAN <54145156+tranvanhoangdv3042002@users.noreply.github.com>
2024-03-31 12:12:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])// "argc" est l'argument count, ça sert à compter le nombre d'arguments
+// dans le command-ligne qui sont passés au program lorsqu'il est exécuté
+// "*argv[]" est le tableau dont les éléments sont des pointeurs sur "char" et ces pointeurs, ils pointent
+// des noms d'arguments et le premier pointeur pointe le nom du programme exécuté.
 {
-    QApplication app(argc, argv);
+    QApplication app(argc, argv); // QApplication est une classe, dans ce cas, cette fonction permet d'initialiser
+    // le système de window et de construire un object d'application
 
     srand(time(0));
 
     SnakeWindow wnd;
     wnd.show();
 
-    return app.exec();
+    return app.exec(); // Execution du programme
 }
 
 
