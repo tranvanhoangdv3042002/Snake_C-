0aab4ae40ccab5ad18ca69c13d7926723cff7c23
M12ria <113218069+M12ria@users.noreply.github.com>
2024-04-12 14:45:04 +0200

    Update snakewindow.cpp

--- snakewindow.cpp
+++ snakewindow.cpp
@@ -25,27 +25,37 @@ SnakeWindow::SnakeWindow(QWidget *pParent, Qt::WindowFlags flags):QFrame(pParent
         cout<<"Impossible d'ouvrir mur.bmp"<<endl;
         exit(-1);
     }
+        if (pixmapFruit.load("./data/orange.png")==false)
+    {
+        cout<<"Impossible d'ouvrir orange.png"<<endl;
+        exit(-1);
+    }
 
     jeu.init();
 
     QTimer *timer = new QTimer(this);
-    connect(timer, &QTimer::timeout, this, &SnakeWindow::handleTimer); // Lorsque le délais de temps est atteint, on va mettre a jour le jeu
-    timer->start(100); // Démarrer le time avec le délais de 100 ms
+    connect(timer, &QTimer::timeout, this, &SnakeWindow::handleTimer);
+    timer->start(100);
+
+    SnakeButton *btnAjout = new SnakeButton(this);
+    btnAjout->setFixedSize(100, 25);
+    btnAjout->setText("Ajout mur");
+    btnAjout->move(10, 10);
+
+    SnakeButton *btnSuppr = new SnakeButton(this);
+    btnSuppr->setFixedSize(100, 25);
+    btnSuppr->setText("Suppr mur");
+    btnSuppr->move(120, 10);
+
+    connect(btnAjout, &QPushButton::clicked, this, &SnakeWindow::handleButtonAjout);
+    connect(btnSuppr, &QPushButton::clicked, this, &SnakeWindow::handleButtonSuppr);
 
     largeurCase = pixmapMur.width();
     hauteurCase = pixmapMur.height();
 
-	// Création des boutons, avec "this" pour les mettre dans la fenêtre principale
-    QPushButton *AjoutMur = new QPushButton ("Ajouter Mur",this);
-    QPushButton *SuppMur = new QPushButton ("Supprimer Mur",this);
-
-    // Etablir la géométrie et la dimension des boutons en haut à gauche
-    AjoutMur->setGeometry(QRect(QPoint(largeurCase, 0),QSize(4*largeurCase,hauteurCase)));
-    SuppMur->setGeometry(QRect(QPoint(largeurCase*5, 0),QSize(4*largeurCase,hauteurCase)));
-
-    resize(jeu.getNbCasesX()*largeurCase, (jeu.getNbCasesY()+1)*hauteurCase); // "Resize" fonction appartient à la classe "QWidget"
-}           // Cette fonction permet de redimensionner la taille de l'écran du jeu
-
+    decalageY = 50;
+    resize(jeu.getNbCasesX()*largeurCase, jeu.getNbCasesY()*hauteurCase+decalageY);
+}
 
 void SnakeWindow::paintEvent(QPaintEvent *)
 {
@@ -63,21 +73,22 @@ void SnakeWindow::paintEvent(QPaintEvent *)
     for (pos.y=0;pos.y<jeu.getNbCasesY();pos.y++)
         for (pos.x=0;pos.x<jeu.getNbCasesX();pos.x++)
 			if (jeu.getCase(pos)==MUR)
-                painter.drawPixmap(pos.x*largeurCase, pos.y*hauteurCase, pixmapMur); // Pour dessiner le MUR
+                painter.drawPixmap(pos.x*largeurCase, pos.y*hauteurCase+decalageY, pixmapMur);
+
     // Dessine le serpent
     const list<Position> &snake = jeu.getSnake();
     if (!snake.empty())
     {
         list<Position>::const_iterator itSnake;
         const Position &posTete = snake.front();
-        painter.drawPixmap(posTete.x*largeurCase, posTete.y*hauteurCase, pixmapTete); // Pour dessiner la tête
+        painter.drawPixmap(posTete.x*largeurCase, posTete.y*hauteurCase+decalageY, pixmapTete);
 
-        for (itSnake=++snake.begin(); itSnake!=snake.end(); itSnake++) // Pour dessiner le corps
-            painter.drawPixmap(itSnake->x*largeurCase, itSnake->y*hauteurCase, pixmapCorps);
+        for (itSnake=++snake.begin(); itSnake!=snake.end(); itSnake++)
+            painter.drawPixmap(itSnake->x*largeurCase, itSnake->y*hauteurCase+decalageY, pixmapCorps);
     }
 }
 
-void SnakeWindow::keyPressEvent(QKeyEvent *event) // Permet de faire le déplacement du serpent
+void SnakeWindow::keyPressEvent(QKeyEvent *event)
 {
     if (event->key()==Qt::Key_Left)
         jeu.setDirection(GAUCHE);
@@ -87,16 +98,15 @@ void SnakeWindow::keyPressEvent(QKeyEvent *event) // Permet de faire le déplace
         jeu.setDirection(HAUT);
     else if (event->key()==Qt::Key_Down)
         jeu.setDirection(BAS);
-	  else if (event->key()==Qt::Key_Backspace)
-        jeu.setDirection(ARRETE);
     update();
 }
 
-void SnakeWindow::handleTimer()  // Mettre a jour le serpent (la position)
+void SnakeWindow::handleTimer()
 {
     jeu.evolue();
     update();
 }
+
 void SnakeWindow::handleButtonAjout()
 {
     jeu.ajoutMur();
