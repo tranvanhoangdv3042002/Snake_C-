914a4e5a9aca59307fb1e1fd1095a7b98ddee8ad
Van Hoang TRAN <54145156+tranvanhoangdv3042002@users.noreply.github.com>
2024-04-16 18:25:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,15 +10,15 @@ int main(int argc, char *argv[])// "argc" est l'argument count, ça sert à comp
 // des noms d'arguments et le premier pointeur pointe le nom du programme exécuté.
 {
     QApplication app(argc, argv); // QApplication est une classe, dans ce cas, cette fonction permet d'initialiser
-    // le système de window et de construire un object d'application
+    // le système de window et de construire un object d'application avec la commande ligne argc dans argv
 
-    srand(time(0));
+    srand(time(0)); // time(0) retourne le nombre de secondes à partir de 00:00 heurs, 01/01/1970 UTC
+                    // Donc, lorsque l'on lance le programme, l'ensemble de nombres aléatoires est généré
+    SnakeWindow wnd; // Une variable de type SnakeWindow crée par défaut
+    wnd.show(); // QWidget::show() permet de représenter le widget et son widget fils
 
-    SnakeWindow wnd;
-    wnd.show();
-
-    return app.exec(); // Execution du programme
+    return app.exec(); // appartenant à la classe "QApplication", la fonction membre statique permet
+                    // d'exécuter le boucle d'événement jusqu'à la fonction "exit()" est appelée
 }
-
 
 
