be43e759bb5082a45c98fb2048c3f271b160e25c
Van Hoang TRAN <54145156+tranvanhoangdv3042002@users.noreply.github.com>
2024-04-16 18:34:36 +0200

    Update snakewindow.hpp

--- snakewindow.hpp
+++ snakewindow.hpp
@@ -5,16 +5,20 @@
 #include <QtWidgets/QtWidgets>
 #include "jeu.hpp"
 
-class SnakeWindow : public QFrame
+class SnakeWindow : public QFrame // Une héritage de la classe QFrame de Qt
 {
   protected:
     Jeu jeu;
-    QPixmap pixmapCorps, pixmapTete, pixmapMur , pixmapFruit;
-    int decalageY;
+    QPixmap pixmapCorps, pixmapTete, pixmapMur, pixmapVide;
+    // QPixmap est une classe héritant de la classe "QPaintDevice" étant le classe de base d'objet
+    // qui peut être dessiné avec "QPainter". "QPainter" founit les fonctions permettant de dessiner GUI
 
   public:
-    SnakeWindow(QWidget *pParent=nullptr, Qt::WindowFlags flags=Qt::WindowFlags());
-    //Position posFruit();
+    SnakeWindow(QWidget *pParent=nullptr, Qt::WindowFlags flags=Qt::WindowFlags()); //Qt::WindowFlags est le type de données (liste d'énumération),
+    // comme Qt::Window, Qt::Dialog, Qt::Widget, Qt::Popup, Qt::Tool, Qt::SplashScreen, Qt::CustomizeWindowHint...
+    // Qt::WindowFlags() : le constructeur par défaut (une fenêtre sans aucun attribut défini)
+    void handleTimer(); // Permet de mettre à jout le jeu dan une intervalle de temps spécifique
+
 
   protected:
     void paintEvent(QPaintEvent *);
